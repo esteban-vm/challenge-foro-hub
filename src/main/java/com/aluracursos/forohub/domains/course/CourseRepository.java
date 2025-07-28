7435cf12a7e6250c8d53e7c525fbfa047c6583fa
Esteban V.M. <estebanvm90@outlook.com>
2025-07-28 13:41:59 -0300

    create CourseRepository

---
+++ src/main/java/com/aluracursos/forohub/domains/course/CourseRepository.java
@@ -0,0 +1,6 @@
+package com.aluracursos.forohub.domains.course;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CourseRepository extends JpaRepository<Course, Long> {
+}
