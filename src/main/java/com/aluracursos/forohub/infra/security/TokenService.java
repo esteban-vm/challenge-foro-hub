815b7a6f38c2e92f6cc4a5698a0ddc50bed98ef6
Esteban V.M. <estebanvm90@outlook.com>
2025-07-29 12:03:57 -0300

    create TokenService

---
+++ src/main/java/com/aluracursos/forohub/infra/security/TokenService.java
@@ -0,0 +1,59 @@
+package com.aluracursos.forohub.infra.security;
+
+import com.aluracursos.forohub.domains.user.User;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
+public class TokenService {
+
+    private static final String ISSUER = "API Foro Hub";
+
+    @Value("${api.security.token.secret}")
+    private String secret;
+
+    public String generateToken(User user) {
+        try {
+            var algorithm = Algorithm.HMAC256(secret);
+
+            return JWT.create()
+                    .withIssuer(ISSUER)
+                    .withSubject(user.getUsername())
+                    .withExpiresAt(expirationDate())
+                    .withClaim("id", user.getId())
+                    .sign(algorithm);
+
+        } catch (JWTCreationException exception) {
+            throw new RuntimeException("Error al generar el token JWT", exception);
+        }
+    }
+
+    private Instant expirationDate() {
+        return LocalDateTime.now()
+                .plusHours(2)
+                .toInstant(ZoneOffset.of("-03:00"));
+    }
+
+    public String getSubject(String jwtToken) {
+        try {
+            var algorithm = Algorithm.HMAC256(secret);
+
+            return JWT.require(algorithm)
+                    .withIssuer(ISSUER)
+                    .build()
+                    .verify(jwtToken)
+                    .getSubject();
+
+        } catch (JWTVerificationException exception) {
+            throw new RuntimeException("Token JWT inválido o expirado", exception);
+        }
+    }
+}
