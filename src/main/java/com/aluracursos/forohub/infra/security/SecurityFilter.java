62863849cb8f2b2b02a3188ff27af29d121aded8
Esteban V.M. <estebanvm90@outlook.com>
2025-07-29 12:02:54 -0300

    create SecurityFilter

---
+++ src/main/java/com/aluracursos/forohub/infra/security/SecurityFilter.java
@@ -0,0 +1,48 @@
+package com.aluracursos.forohub.infra.security;
+
+import com.aluracursos.forohub.domains.user.UserRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Autowired
+    private TokenService service;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        var jwtToken = getToken(request);
+
+        if (jwtToken != null) {
+            var subject = service.getSubject(jwtToken);
+            var user = repository.findByEmail(subject);
+            var authToken = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authToken);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String getToken(HttpServletRequest request) {
+        var authHeader = request.getHeader("Authorization");
+
+        if (authHeader != null) {
+            return authHeader.replace("Bearer ", "").trim();
+        }
+
+        return null;
+    }
+}
