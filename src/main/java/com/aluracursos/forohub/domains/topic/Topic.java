c62adf853d46abfd37333d7e4db147f54732b229
Esteban V.M. <estebanvm90@outlook.com>
2025-08-13 13:47:46 -0300

    update Topic

--- src/main/java/com/aluracursos/forohub/domains/topic/Topic.java
+++ src/main/java/com/aluracursos/forohub/domains/topic/Topic.java
@@ -3,6 +3,7 @@ package com.aluracursos.forohub.domains.topic;
 import com.aluracursos.forohub.domains.course.Course;
 import com.aluracursos.forohub.domains.user.User;
 import jakarta.persistence.*;
+import jakarta.validation.Valid;
 import lombok.*;
 
 import java.time.LocalDateTime;
@@ -41,4 +42,27 @@ public class Topic {
 
     @Column(name = "updated_at")
     private LocalDateTime updatedAt;
+
+    public Topic(TopicRegisterData data, User author, Course course) {
+        this.title = data.title();
+        this.message = data.message();
+        this.author = author;
+        this.course = course;
+    }
+
+    @PrePersist
+    public void prePersist() {
+        this.status = TopicStatus.ACTIVE;
+    }
+
+    public void update(@Valid TopicUpdateData data) {
+        if (data.title() != null) {
+            this.title = data.title();
+        }
+
+        if (data.message() != null) {
+            this.message = data.message();
+        }
+    }
+    
 }
