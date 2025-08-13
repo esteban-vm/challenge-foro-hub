18c2684218e2e07fe81f7d3d0af6343512f12416
Esteban V.M. <estebanvm90@outlook.com>
2025-08-13 13:47:07 -0300

    update TopicDetailData

--- src/main/java/com/aluracursos/forohub/domains/topic/TopicDetailData.java
+++ src/main/java/com/aluracursos/forohub/domains/topic/TopicDetailData.java
@@ -1,4 +1,31 @@
 package com.aluracursos.forohub.domains.topic;
 
-public record TopicDetailData() {
+import java.time.LocalDateTime;
+
+public record TopicDetailData(
+
+        Long id,
+        String title,
+        String message,
+        TopicStatus status,
+        String author,
+        String course,
+        LocalDateTime createdAt,
+        LocalDateTime updatedAt
+
+) {
+
+    public TopicDetailData(Topic topic) {
+        this(
+                topic.getId(),
+                topic.getTitle(),
+                topic.getMessage(),
+                topic.getStatus(),
+                topic.getAuthor().getProfile().getName(),
+                topic.getCourse().getTitle(),
+                topic.getCreatedAt(),
+                topic.getUpdatedAt()
+        );
+    }
+
 }
