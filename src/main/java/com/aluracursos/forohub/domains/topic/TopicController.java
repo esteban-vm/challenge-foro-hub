ab182627034fd07576ab1ef13549839dfe58e27c
Esteban V.M. <estebanvm90@outlook.com>
2025-08-13 13:47:34 -0300

    update TopicController

--- src/main/java/com/aluracursos/forohub/domains/topic/TopicController.java
+++ src/main/java/com/aluracursos/forohub/domains/topic/TopicController.java
@@ -1,11 +1,14 @@
 package com.aluracursos.forohub.domains.topic;
 
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping("/topics")
@@ -14,9 +17,42 @@ public class TopicController {
     @Autowired
     private TopicService service;
 
+    @GetMapping
+    public ResponseEntity<Page<TopicDetailData>> getAll(@PageableDefault(size = 15, sort = {"createdAt"}) Pageable pageable) {
+        var topics = service.getAll(pageable);
+        return ResponseEntity.ok(topics);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<TopicDetailData> getOne(@PathVariable Long id) {
+        var topic = service.getOne(id);
+        return ResponseEntity.ok(topic);
+    }
+
     @Transactional
     @PostMapping
-    public ResponseEntity<Void> register() {
-        return null;
+    public ResponseEntity<TopicDetailData> register(@RequestBody @Valid TopicRegisterData data, UriComponentsBuilder builder) {
+        var topic = service.register(data);
+        var uri = builder.path("/topics/{id}").buildAndExpand(topic.id()).toUri();
+        return ResponseEntity.created(uri).body(topic);
     }
+
+    @Transactional
+    @PutMapping("/{id}")
+    public ResponseEntity<TopicDetailData> update(@PathVariable Long id, @RequestBody @Valid TopicUpdateData data) {
+        var topic = service.update(id, data);
+
+        if (topic == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok(topic);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
 }
