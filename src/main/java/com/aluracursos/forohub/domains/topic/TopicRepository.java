996acbcaed1b920cc5f0d397ccdcd7370f8b52a1
Esteban V.M. <estebanvm90@outlook.com>
2025-08-13 13:46:47 -0300

    update TopicRepository

--- src/main/java/com/aluracursos/forohub/domains/topic/TopicRepository.java
+++ src/main/java/com/aluracursos/forohub/domains/topic/TopicRepository.java
@@ -1,6 +1,11 @@
 package com.aluracursos.forohub.domains.topic;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TopicRepository extends JpaRepository<Topic, Long> {
+
+    Page<Topic> findAllByStatusActive(Pageable pageable);
+
 }
