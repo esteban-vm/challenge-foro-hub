b71ebf77a62b7e4835ecc9f7351fa7b37e4ad393
Esteban V.M. <estebanvm90@outlook.com>
2025-08-13 13:20:50 -0300

    update TopicUpdateData

--- src/main/java/com/aluracursos/forohub/domains/topic/TopicUpdateData.java
+++ src/main/java/com/aluracursos/forohub/domains/topic/TopicUpdateData.java
@@ -1,11 +1,7 @@
 package com.aluracursos.forohub.domains.topic;
 
-import jakarta.validation.constraints.NotNull;
-
 public record TopicUpdateData(
 
-        @NotNull(message = "Se requiere el identificador del tópico")
-        Long id,
         String title,
         String message
 
