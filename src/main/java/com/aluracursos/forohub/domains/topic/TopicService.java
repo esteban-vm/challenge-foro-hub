1c5138f11b4288f08d2e9645adf64a989b7a7b4d
Esteban V.M. <estebanvm90@outlook.com>
2025-08-13 13:46:35 -0300

    update TopicService

--- src/main/java/com/aluracursos/forohub/domains/topic/TopicService.java
+++ src/main/java/com/aluracursos/forohub/domains/topic/TopicService.java
@@ -3,6 +3,8 @@ package com.aluracursos.forohub.domains.topic;
 import com.aluracursos.forohub.domains.course.CourseRepository;
 import com.aluracursos.forohub.domains.user.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -17,8 +19,41 @@ public class TopicService {
     @Autowired
     private TopicRepository topicRepository;
 
+    public Page<TopicDetailData> getAll(Pageable pageable) {
+        return topicRepository.findAllByStatusActive(pageable).map(TopicDetailData::new);
+    }
+
+    public TopicDetailData getOne(Long id) {
+        var topic = topicRepository.getReferenceById(id);
+        return new TopicDetailData(topic);
+    }
+
     public TopicDetailData register(TopicRegisterData data) {
-        var topic = new Topic(data);
+        var author = userRepository.getReferenceById(data.authorId());
+        var course = courseRepository.getReferenceById(data.courseId());
+        var topic = new Topic(data, author, course);
+        topicRepository.save(topic);
+        return new TopicDetailData(topic);
+    }
+
+    public TopicDetailData update(Long id, TopicUpdateData data) {
+        var optTopic = topicRepository.findById(id);
+
+        if (optTopic.isPresent()) {
+            var topic = optTopic.get();
+
+            if (topic.getStatus() == TopicStatus.ACTIVE) {
+                topic.update(data);
+                return new TopicDetailData(topic);
+            }
+        }
+
         return null;
     }
+
+    public void delete(Long id) {
+        var optTopic = topicRepository.findById(id);
+        optTopic.ifPresent(topic -> topicRepository.deleteById(topic.getId()));
+    }
+
 }
