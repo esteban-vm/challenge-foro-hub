2e6bd12947eeb29495d5af22435aa98e6fa6ebbc
Esteban V.M. <estebanvm90@outlook.com>
2025-08-13 09:35:24 -0300

    update TopicRegisterData

--- src/main/java/com/aluracursos/forohub/domains/topic/TopicRegisterData.java
+++ src/main/java/com/aluracursos/forohub/domains/topic/TopicRegisterData.java
@@ -1,9 +1,18 @@
 package com.aluracursos.forohub.domains.topic;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 public record TopicRegisterData(
+
+        @NotBlank(message = "Se requiere el título")
         String title,
+        @NotBlank(message = "Se requiere el mensaje")
         String message,
+        @NotNull(message = "Se requiere el identificador del autor")
         Long authorId,
+        @NotNull(message = "Se requiere el identificador del curso")
         Long courseId
+        
 ) {
 }
